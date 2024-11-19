8dd780f9dc47b7fc834e6d44721b8d542a8d5746
Juan <jc11093@gmail.com>
2024-11-18 21:25:35 -0500

    .

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.getElementById('registroForm').addEventListener('submit', function(event){
-    const nombre= document.getElementById('nombre').value.trim();
-    const correo= document.getElementById('correo').value.trim();
-    const imagen= document.getElementById('imagen').value();
+    alert("llega linea 2");
+    let nombre = document.getElementById('nombre').value.trim();
+    let correo = document.getElementById('correo').value.trim();
+   // let imagen = document.getElementById('imagen').value();
 
-    if(!nombre=="" || correo=="" || imagen==""){
+    alert("llega linea 6");
+    console.log("esta es la captura");
+    console.log(nombre);
+
+    if(nombre==""){
         alert('Por favor, completa todos los campos obligatorios.');
         event.preventDefault();
         return;
@@ -11,6 +16,7 @@ document.getElementById('registroForm').addEventListener('submit', function(even
 
 
 });
+
 function mostrarImagen(event){
     let imagen=document.getElementById('ver-imagen');
     imagen.src=URL.createObjectURL(event.target.files[0]);
