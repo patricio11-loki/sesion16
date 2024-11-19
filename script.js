a29798db43762ce4903eb2961249344f2a6f3266
Juan <jc11093@gmail.com>
2024-11-18 21:34:58 -0500

    -

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-document.getElementById('registroForm').addEventListener('submit', function(event){
-    alert("llega linea 2");
-    let nombre = document.getElementById('nombre').value.trim();
-    let correo = document.getElementById('correo').value.trim();
-   // let imagen = document.getElementById('imagen').value();
+document.getElementById('registroForm').addEventListener('submit', function(event) {
+    const nombre = document.getElementById('nombre').value.trim();
+    const correo = document.getElementById('correo').value.trim();
+    const imagen = document.getElementById('imagen').value;
 
-    alert("llega linea 6");
-    console.log("esta es la captura");
-    console.log(nombre);
-
-    if(nombre==""){
-        alert('Por favor, completa todos los campos obligatorios.');
-        event.preventDefault();
-        return;
-    }
-
-
+    if (nombre=="" || correo=="" || imagen=="") {
+        alert("Por favor, completa todos los campos obligatorios.");
+        return false;
+   }
+return true;
 });
-
-function mostrarImagen(event){
-    let imagen=document.getElementById('ver-imagen');
-    imagen.src=URL.createObjectURL(event.target.files[0]);
-    }
+function mostrarImagen(event) {
+    const imagen = document.getElementById('ver-imagen');
+    imagen.src = URL.createObjectURL(event.target.files[0]);
+}
