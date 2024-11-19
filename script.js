74aaa93b07f46e861f784317077d34847ac713d9
Juan <jc11093@gmail.com>
2024-11-18 20:34:09 -0500

    prueba

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.get
+document.getElementById('registroForm').addEventListener('submit', function(event){
+    const nombre= document.getElementById('nombre').ariaValueMax.trim();
+    const correo= document.getElementById('correo').ariaValueMax.trim();
+    const imagen= document.getElementById('nombre').ariaValueMax.trim();
+
+
+});
+
 
 function mostrarImagen(event){
     let imagen=document.getElementById('ver-imagen');
